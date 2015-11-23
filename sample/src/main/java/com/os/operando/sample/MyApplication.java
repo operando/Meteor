6a4e1f22f1d56945b7bd0fc9ffb97465e7dd91e4
Shinobu Okano Mac <okanoshinobu8@gmail.com>
2015-11-23 19:10:49 +0900

    Rename sample

---
+++ sample/src/main/java/com/os/operando/sample/MyApplication.java
@@ -0,0 +1,12 @@
+package com.os.operando.sample;
+
+import android.app.Application;
+
+public class MyApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+}
