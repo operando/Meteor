d841a3aab6cd1e0677df400d7a74583a80cf4192
operando <okanoshinobu8@gmail.com>
2015-11-24 20:17:20 +0900

    Fix sample code

--- sample/src/main/java/com/os/operando/sample/MyApplication.java
+++ sample/src/main/java/com/os/operando/sample/MyApplication.java
@@ -3,6 +3,7 @@ package com.os.operando.sample;
 import android.app.Application;
 
 import com.os.operando.meteor.ComponentType;
+import com.os.operando.meteor.Meteor;
 import com.os.operando.meteor.MeteorActivityLifecycleCallbacks;
 import com.os.operando.meteor.MeteorNotification;
 
@@ -11,6 +12,7 @@ public class MyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        Meteor.init(this, SlackPostIntentService.createComponentName(this), ComponentType.Service);
         MeteorNotification.show(this, SlackPostIntentService.createComponentName(this), ComponentType.Service);
         registerActivityLifecycleCallbacks(new MeteorActivityLifecycleCallbacks());
     }
